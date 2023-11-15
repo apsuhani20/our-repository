43416a459a918ffffad50ce688d61c53c4dea919
HymaJujjuru <143463804+HymaJujjuru@users.noreply.github.com>
2023-11-15 16:06:42 -0700

    About Me Update 1

--- src/pages/HymaJujjuru.js
+++ src/pages/HymaJujjuru.js
@@ -6,23 +6,21 @@ export default function examplePage() {
 
         <main>
           <div className='underline'>
-            <h1 className='title'>Hey, I'm Spider man. </h1>
+            <h1 className='title'>Hi, This is Hyma. </h1>
           </div>
           <img
             className='pic'
             src='https://i.postimg.cc/qR2VgDkd/spiderman.jpg'
-            alt='tom holland'
+            alt="Hyma's Picture"
           />
 
           <p className='description'>
-            Hey I am your friendly neighbourhood Spider Man. My real name is Tom
-            Holland. I am an actor but mostly I fight villains and juggle high school.
-            I am just your average student – well, maybe not so average. 
-            You see, I've got some incredible powers, like superhuman strength, agility, 
-            and the ability to shoot webs from my wrists. With great power comes great 
-            responsibility, and I'm here to protect New York City from all kinds of baddies,
-            from common crooks to supervillains. So, if you ever need a hand or a web-swinging
-            hero in your corner, just remember, I'm your friendly neighborhood Spider-Man!"
+            Hi! My name is Hyma Jujjuru. I am a Computer Science major studying at CU Boulder. 
+            I am very excited to learn the various aspects and fields of computer science. My goal
+            is to work on Artificial Intelligence and/or Machine Learning. My favorite hobbies are
+            reading books, listening to music, hanging out with friends, and learning new things.
+            I love travelling! I hope that I can travel many countries in the future and possibly
+            do a study abroad program in countries like Japan or France. 
           </p>
 
           <a className='button' href='/'>
